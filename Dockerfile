0e95041c2db749a764588b6ab6ccd1b558880e21
Charmunks <im@char.best>
2026-01-12 13:06:28 -0500

    fix

--- Dockerfile
+++ Dockerfile
@@ -21,4 +21,4 @@ ENV PORT=3000
 
 EXPOSE 3000
 
-CMD ["node", "build"]
+CMD ["node", "build/index.js"]
--- src/routes/page/[page]/+page.js
+++ src/routes/page/[page]/+page.js
@@ -1,17 +1,20 @@
+const modules = import.meta.glob('$lib/content/*.svx');
+
 export async function load({ params }) {
 	const { page } = params;
+	const path = `/src/lib/content/${page}.svx`;
 	
-	try {
-		const content = await import(`$lib/content/${page}.svx`);
+	if (modules[path]) {
+		const content = await modules[path]();
 		return {
 			Content: content.default,
 			page
 		};
-	} catch (e) {
-		return {
-			Content: null,
-			page,
-			error: 'Page not found'
-		};
 	}
+	
+	return {
+		Content: null,
+		page,
+		error: 'Page not found'
+	};
 }
